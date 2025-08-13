2850e0f416e0c35f6ec2675c1962f837734dc8e7
El <elchananavraham.t@gmail.com>
2025-08-13 20:52:23 +0300

    Add animation to almost all of the components, just the footer left

--- src/components/PreFooter.tsx
+++ src/components/PreFooter.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import Button from "./Button.tsx";
 import Glowing from "./Glowing.tsx";
 
@@ -5,14 +6,46 @@ const PreFooter = () => {
     return (
         <div className="flex flex-col justify-center items-center text-wrap text-center text-white overflow-hidden py-16 px-6">
 
-            <div className="relative">
+            <div>
                 <Glowing x={-400} y={-500} />
             </div>
 
             <div className="md:w-3/5 flex flex-col items-center gap-4">
-                <h2 className="text-5xl">Ready to take control of your crypto?</h2>
-                <p className="my-6 md:mb-2 md:w-12/13">Join thousands of users who trust Cryptix for secure, seamless, and efficient cryptocurrency transactions. Start now and unlock the full potential of digital assets.</p>
-                <Button title="Get started now" variant="primary" />
+                <motion.h2
+                    initial={{ opacity: 0, y: -20 }}
+                    whileInView={{ opacity: 1, y:0 }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 500,
+                        damping: 20,
+                        mass: 0.8
+                    }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    className="text-5xl">Ready to take control of your crypto?</motion.h2>
+                <motion.p
+                    initial={{ opacity: 0, scale: 0.9 }}
+                    whileInView={{ opacity: 1, scale: 1.1 }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 500,
+                        damping: 20,
+                        mass: 0.8
+                    }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    className="my-6 md:mb-2 md:w-12/13">Join thousands of users who trust Cryptix for secure, seamless, and efficient cryptocurrency transactions. Start now and unlock the full potential of digital assets.</motion.p>
+                <motion.span
+                    initial={{ opacity: 0, y: 20 }}
+                    whileInView={{ opacity: 1, y:0 }}
+                    transition={{
+                        type: "spring",
+                        stiffness: 500,
+                        damping: 20,
+                        mass: 0.8
+                    }}
+                    viewport={{ once: true, amount: 0.3 }}
+                >
+                    <Button title="Get started now" variant="primary" />
+                </motion.span>
             </div>
 
             <div className="relative">
