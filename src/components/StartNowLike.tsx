80ed1f0b4c3b9b95fc41dd877f6bed730f1dcb94
El <elchananavraham.t@gmail.com>
2025-08-13 19:52:56 +0300

    Add animation to the StartNowLike component

--- src/components/StartNowLike.tsx
+++ src/components/StartNowLike.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import Button from "./Button.tsx";
 
 interface StartNowLikeProps {
@@ -10,14 +11,46 @@ const StartNowLike = ({ heading, paragraph }: StartNowLikeProps) => {
         <div className="w-full text-white flex flex-col md:flex-row">
             <div className="md:w-2/3 flex flex-col justify-between">
                 <div className="max-md:text-center p-6">
-                    <h2 className="text-4xl mb-4">{heading}</h2>
-                    <p className="text-wrap text-lg">{paragraph}</p>
+                    <motion.h2
+                        initial={{ x: -200, opacity: 0 }}
+                        whileInView={{ x: 0, opacity: 1 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 500,
+                            damping: 20,
+                            mass: 0.8
+                        }}
+                        viewport={{ once: true, amount: 0.3 }}
+                        className="text-4xl mb-4">{heading}</motion.h2>
+                    <motion.p
+                        initial={{ x: -200, opacity: 0 }}
+                        whileInView={{ x: 0, opacity: 1 }}
+                        transition={{
+                            type: "spring",
+                            stiffness: 500,
+                            damping: 20,
+                            mass: 0.8,
+                            delay: 0.2
+                        }}
+                        viewport={{ once: true, amount: 0.3 }}
+                        className="text-wrap text-lg">{paragraph}</motion.p>
                 </div>
             </div>
-            <div className="md:w-1/3 md:border-l border-gray-600/70">
+            <motion.div
+                initial={{ x: -500, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{
+                    type: "spring",
+                    stiffness: 500,
+                    damping: 20,
+                    mass: 0.8,
+                    delay: 0.5
+                }}
+                viewport={{ once: true, amount: 0.3 }}
+                className="md:w-1/3 md:border-l border-gray-600/70">
                 <div className="h-1/2"/>
                 <Button title="Create account now" variant='secondary' right={true}/>
-            </div>
+            </motion.div>
         </div>
     )
 }
